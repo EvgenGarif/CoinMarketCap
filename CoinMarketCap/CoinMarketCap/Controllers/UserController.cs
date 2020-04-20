2ceab976f10a6537648120327f2eee2d34f46b49
EvgenGarif <aldan2313@gmail.com>
2020-04-20 14:46:20 +0500

    first commit

---
+++ CoinMarketCap/CoinMarketCap/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using CoinMarketCap.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+
+namespace CoinMarketCap.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize]
+        public ActionResult MarketCapInfo()
+        {
+            string data = "";
+            List<MarketCap> MarketCapList = new List<MarketCap>();
+            try
+            {
+                data = makeAPICall();
+                JObject obj = JObject.Parse(data);
+
+                foreach (var val in obj.SelectToken("data"))
+                {
+                    MarketCap marketCap = new MarketCap();
+
+                    marketCap.Name = val.SelectToken("name").ToString();
+                   
+                    marketCap.Symbol = val.SelectToken("symbol").ToString();
+                    
+                
+                    decimal price = Convert.ToDecimal(val.SelectToken("quote.USD.price"));
+                    marketCap.Price = "$" + Math.Round(price, 2);
+
+                    decimal changeHour = Convert.ToDecimal(val.SelectToken("quote.USD.percent_change_1h"));
+                    marketCap.ChangeHour = Math.Round(changeHour, 2) + "%";
+
+                    decimal changeDay = Convert.ToDecimal(val.SelectToken("quote.USD.percent_change_24h"));
+                    marketCap.ChangeDay = Math.Round(changeDay, 2) + "%";
+
+                    decimal marketCapValue = Convert.ToDecimal(val.SelectToken("quote.USD.market_cap"));
+                    marketCap.MarketCapValue = "$" + Math.Round(marketCapValue, 2);
+
+                    var iconUrl = ConfigurationManager.AppSettings.Get("iconUrl");
+                    var icon = val.SelectToken("id").ToString() + ".png";
+                    iconUrl += icon;
+                    marketCap.Icon = iconUrl;
+
+                    string lastUpdated = Convert.ToString(val.SelectToken("quote.USD.last_updated"));
+                    marketCap.LastUpdated = lastUpdated;
+
+                    MarketCapList.Add(marketCap);
+                }
+
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return View("MarketCapList", MarketCapList);
+        }
+
+        public string makeAPICall()
+        {
+            string API_KEY = ConfigurationManager.AppSettings.Get("api_key"); 
+            var URL = new UriBuilder(ConfigurationManager.AppSettings.Get("url"));
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = ConfigurationManager.AppSettings.Get("start");  
+            queryString["limit"] = ConfigurationManager.AppSettings.Get("limit"); 
+            queryString["convert"] = ConfigurationManager.AppSettings.Get("convert");
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            return client.DownloadString(URL.ToString());
+        }
+    }
+}
---
+++ CoinMarketCap/CoinMarketCap/Models/MarketCap.cs
@@ -0,0 +1,15 @@
+﻿namespace CoinMarketCap.Models
+{
+    public class MarketCap
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Icon { get; set; }
+        public string ChangeHour { get; set; }
+        public string ChangeDay { get; set; }
+        public string MarketCapValue { get; set; }
+        public string Price { get; set; }
+        public string LastUpdated { get; set; }
+
+    }
+}
